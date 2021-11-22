6f1e68cd8f4ea6bc23526120135425a8a49e2319
Sergey <svh.serjo@gmail.com>
2021-11-22 10:51:40 +0300

    5.6 homework

--- main.py
+++ main.py
@@ -1,86 +1,74 @@
-def greet():
-    print("-------------------")
-    print("  Приветсвуем вас  ")
-    print("      в игре       ")
-    print("  крестики-нолики  ")
-    print("-------------------")
-    print(" формат ввода: x y ")
-    print(" x - номер строки  ")
-    print(" y - номер столбца ")
+board = list(range(1, 10))
 
-def show():
-    print()
-    print("    | 0 | 1 | 2 | ")
-    print("  --------------- ")
-    for i, row in enumerate(field):
-        row_str = f"  {i} | {' | '.join(row)} | "
-        print(row_str)
-        print("  --------------- ")
-    print()
 
-def ask():
+def welcome():
+    print("Добро пожаловать в игру")
+    print("    крестики-нолики!    ")
+    print("   Для выигрыша вы должны")
+    print("выстроить три крестика или ")
+    print("нолика в ряд. Ходить пооче-")
+    print("рёдно до победы, указывая ")
+    print("номер поля согласно шаблону")
+    print("ниже.")
+
+
+def print_board(board):
+    print("-------------")
+    for i in range(3):
+        print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
+        print("-------------")
+
+
+def turn_input(player_flag):
     while True:
-        cords = input("         Ваш ход: ").split()
-        
-        if len(cords) != 2:
-            print(" Введите 2 координаты! ")
+        turn_value = input("Куда поставим " + player_flag + "? ")
+        if not (turn_value.isdigit()):
+            print("Символ не распознан, введите число")
             continue
-        
-        x, y = cords
-        
-        if not(x.isdigit()) or not(y.isdigit()):
-            print(" Введите числа! ")
+
+        turn_value = int(turn_value)
+
+        if 1 > turn_value or turn_value > 9:
+            print("Значение не распознано. Введите число от 1 до 9.")
             continue
-        
-        x, y = int(x), int(y)
-        
-        if 0 > x or x > 2 or  0 > y or  y > 2 :
-            print(" Координаты вне диапазона! ")
+
+        if str(board[turn_value - 1]) in "XO":
+            print("Эта клетка уже занята, введите номер другой клетки.")
             continue
-        
-        if field[x][y] != " ":
-            print(" Клетка занята! ")
-            continue
-        
-        return x, y
-            
-def check_win():
-    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
-                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
-                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
-    for cord in win_cord:
-        symbols = []
-        for c in cord:
-            symbols.append(field[c[0]][c[1]])
-        if symbols == ["X", "X", "X"]:
-            print("Выиграл X!!!")
-            return True
-        if symbols == ["0", "0", "0"]:
-            print("Выиграл 0!!!")
-            return True
+        board[turn_value - 1] = player_flag
+        print_board(board)
+        return turn_value
+
+
+def win_check(board):
+    win_line = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for field in win_line:
+        if board[field[0]] == board[field[1]] == board[field[2]]:
+            print("Выиграл " + str(board[field[1]]) + "!")
+            return board[field[1]]
     return False
 
-greet()
-field = [[" "] * 3 for i in range(3) ]
-count = 0
-while True:
-    count += 1
-    show()
-    if count % 2 == 1:
-        print(" Ходит крестик!")
-    else:
-        print(" Ходит нолик!")
-    
-    x, y = ask()
-    
-    if count % 2 == 1:
-        field[x][y] = "X"
-    else:
-        field[x][y] = "0"
-    
-    if check_win():
-        break
-    
-    if count == 9:
-        print(" Ничья!")
-        break
+
+def main(board):
+    welcome()
+    print_board(board)
+    win = False
+    turn_counter = 0
+    while not win:
+        if turn_counter % 2 == 0:
+            turn_input("X")
+        else:
+            turn_input("0")
+        turn_counter += 1
+        if turn_counter > 4:
+            tmp = win_check(board)
+            if tmp:
+                break
+        if turn_counter == 9:
+            print("Ничья!")
+            break
+    print_board(board)
+
+
+main(board)
+
